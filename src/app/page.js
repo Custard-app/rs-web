db6757c1368341cb0243f73b4bff5f4f11eb5220
Shubham <shubhamkumar7112002@gmail.com>
2024-11-09 12:02:52 +0530

    testing

--- src/app/page.js
+++ src/app/page.js
@@ -20,6 +20,7 @@ export default function Home() {
             <p className="w-full text-right text-lg font-lato font-medium">
               ~ Wolf of Wall Street
             </p>
+            <p className='text-4xl font-bold'>Hello world</p>
           </div>
         </div>
         <Dreamsection />
