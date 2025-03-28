547840207d74e5564580f5a7da7968f8bc549008
Shubham <shubhamkumar7112002@gmail.com>
2025-03-28 15:22:23 +0530

    fixed-1

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
+// console.log("ApiKey", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+// console.log("AuthDomain", process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
+// console.log("ProjectId", process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID);
+// console.log("StorageBucket", process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET);
+// console.log("MessagingSenderId", process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID);
+// console.log("AppId", process.env.NEXT_PUBLIC_FIREBASE_APP_ID);
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
